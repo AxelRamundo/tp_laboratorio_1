3bcfc6f4bef74d4489b389e26a09e1abcc994739
AxelRamundo <AxelRamundo@gmail.com.ar>
2016-04-07 21:06:28 -0300

    tp_laboratorio_1

---
+++ TP1/Tp1_Laboratorio_1/funciones.h
@@ -0,0 +1,78 @@
+#ifndef FUNCIONES_H_INCLUDED
+#define FUNCIONES_H_INCLUDED
+
+int num1(x)
+{
+    /*REALIZAMOS EN INGRESO DEL 1ER OPERANDO*/
+    printf("Ingrese el 1er operando: ");
+    scanf("%d",&x);
+    system("cls");
+    return x;
+}
+
+int num2 (y)
+{
+    /*REALIZAMOS EL INGRESO DEL 2DO OPERANDO*/
+    printf("Ingrese el 2do operando: ");
+    scanf("%d",&y);
+    system("cls");
+    return y;
+}
+
+void suma (x,y)
+{
+    /*SUMAMOS AMBOS OPERANDOS*/
+    printf("El resultado de la suma es: %d\n",x+y);
+}
+
+void resta (x,y)
+{
+    /*RESTAMOS AMBOS OPERANDOS*/
+    printf("El resultado de la resta es: %d\n",x-y);
+}
+
+void division (x,y)
+{
+    /*VERIFICAMOS QUE EL DIVISOR (2DO OPERANDO) NO SEA 0*/
+    if(y==0)
+    {
+        printf("Imposible dividir por 0\n");
+    }
+    /*SI EL DIVISOR (2DO OPERANDO) ES DISTINTO A 0 PROCEDEMOS CON LA DIVISION*/
+    else
+    {
+        printf("El resultado de la division es: %d\n",x/y);
+    }
+}
+
+void multiplicacion (x,y)
+{
+    /*MULTIPLICAMOS AMBOS OPERANDOS*/
+    printf("El resultado de la multiplicacion es: %d\n",x*y);
+}
+
+void factorial (x)
+{
+    /*BUSCAMOS EL NUMERO FACTORIAL DEL PRIMER OPERANDO*/
+    int i=x;
+    double fac=1;
+    while(i!=0)
+    {
+        fac=fac*i;
+        i--;
+    }
+    printf("El numero factorial de %d es: %lg\n",x,fac);
+}
+
+void error()
+{
+    /*SI LA OPCION INGRESADA NO ES VALIDA MOSTRAMOS UN MENSAJE DE ERROR Y VOLVEMOS AL MENU*/
+    system("cls");
+    printf("===============ERROR================\n");
+    printf("= LA OPCION INGRESADA NO ES VALIDA =\n");
+    printf("====================================\n");
+    system("pause");
+    system("cls");
+}
+#endif // FUNCIONES_H_INCLUDED
+
